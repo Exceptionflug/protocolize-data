2826a438b13cb27ee4d76fa2c357ef5dc7cc7e3d
Leonhard S <49833472+KotlinFactory@users.noreply.github.com>
2022-11-18 23:49:32 +0100

    Sync protocolize-data to latest changes in protocolize-api

--- protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/BlockPlacementListener.java
+++ protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/BlockPlacementListener.java
@@ -3,10 +3,8 @@ package dev.simplix.protocolize.data.listeners;
 import dev.simplix.protocolize.api.Direction;
 import dev.simplix.protocolize.api.PlayerInteract;
 import dev.simplix.protocolize.api.inventory.PlayerInventory;
-import dev.simplix.protocolize.api.item.ItemStack;
-import dev.simplix.protocolize.api.listener.AbstractPacketListener;
-import dev.simplix.protocolize.api.listener.PacketReceiveEvent;
-import dev.simplix.protocolize.api.listener.PacketSendEvent;
+import dev.simplix.protocolize.api.item.BaseItemStack;
+import dev.simplix.protocolize.api.listener.*;
 import dev.simplix.protocolize.data.packets.BlockPlacement;
 
 /**
@@ -26,7 +24,7 @@ public final class BlockPlacementListener extends AbstractPacketListener<BlockPl
             return;
         }
         PlayerInventory playerInventory = event.player().proxyInventory();
-        ItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
+        BaseItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
         PlayerInteract interact = new PlayerInteract(inHand, event.packet().position(), event.packet().hand(), false);
         event.player().handleInteract(interact);
         if (!event.packet().position().equals(interact.clickedBlockPosition())) {
--- protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/UseItemListener.java
+++ protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/UseItemListener.java
@@ -3,6 +3,7 @@ package dev.simplix.protocolize.data.listeners;
 import dev.simplix.protocolize.api.Direction;
 import dev.simplix.protocolize.api.PlayerInteract;
 import dev.simplix.protocolize.api.inventory.PlayerInventory;
+import dev.simplix.protocolize.api.item.BaseItemStack;
 import dev.simplix.protocolize.api.item.ItemStack;
 import dev.simplix.protocolize.api.listener.AbstractPacketListener;
 import dev.simplix.protocolize.api.listener.PacketReceiveEvent;
@@ -26,7 +27,7 @@ public final class UseItemListener extends AbstractPacketListener<UseItem> {
             return;
         }
         PlayerInventory playerInventory = event.player().proxyInventory();
-        ItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
+        BaseItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
         PlayerInteract interact = new PlayerInteract(inHand, null, event.packet().hand(), false);
         event.player().handleInteract(interact);
         if (event.packet().hand() != interact.hand()) {
