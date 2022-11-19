09cf0bd8fc487850efadabc0e2294a81323ef4d2
Leonhard S <49833472+KotlinFactory@users.noreply.github.com>
2022-11-19 18:54:10 +0100

    Update BlockPlacementListener.java

--- protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/BlockPlacementListener.java
+++ protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/listeners/BlockPlacementListener.java
@@ -4,7 +4,9 @@ import dev.simplix.protocolize.api.Direction;
 import dev.simplix.protocolize.api.PlayerInteract;
 import dev.simplix.protocolize.api.inventory.PlayerInventory;
 import dev.simplix.protocolize.api.item.BaseItemStack;
-import dev.simplix.protocolize.api.listener.*;
+import dev.simplix.protocolize.api.listener.AbstractPacketListener;
+import dev.simplix.protocolize.api.listener.PacketReceiveEvent;
+import dev.simplix.protocolize.api.listener.PacketSendEvent;
 import dev.simplix.protocolize.data.packets.BlockPlacement;
 
 /**
@@ -23,20 +25,24 @@ public final class BlockPlacementListener extends AbstractPacketListener<BlockPl
         if (event.player() == null) {
             return;
         }
-        PlayerInventory playerInventory = event.player().proxyInventory();
-        BaseItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
-        PlayerInteract interact = new PlayerInteract(inHand, event.packet().position(), event.packet().hand(), false);
-        event.player().handleInteract(interact);
-        if (!event.packet().position().equals(interact.clickedBlockPosition())) {
-            event.packet().position(interact.clickedBlockPosition());
-            event.markForRewrite();
-        }
-        if (event.packet().hand() != interact.hand()) {
-            event.packet().hand(interact.hand());
-            event.markForRewrite();
-        }
-        if (inHand != null || interact.cancelled()) {
-            event.cancelled(true);
+        try {
+            PlayerInventory playerInventory = event.player().proxyInventory();
+            BaseItemStack inHand = playerInventory.item(playerInventory.heldItem() + 36);
+            PlayerInteract interact = new PlayerInteract(inHand, event.packet().position(), event.packet().hand(), false);
+            event.player().handleInteract(interact);
+            if (!event.packet().position().equals(interact.clickedBlockPosition())) {
+                event.packet().position(interact.clickedBlockPosition());
+                event.markForRewrite();
+            }
+            if (event.packet().hand() != interact.hand()) {
+                event.packet().hand(interact.hand());
+                event.markForRewrite();
+            }
+            if (inHand != null || interact.cancelled()) {
+                event.cancelled(true);
+            }
+        } catch (Throwable ignored) {
+
         }
     }
 
