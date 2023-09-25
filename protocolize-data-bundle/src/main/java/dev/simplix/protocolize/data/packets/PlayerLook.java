829dc3206fae7d1a32b437f34205bd7b7fd4a466
Nico <n.britze@exceptionflug.de>
2023-09-25 11:58:53 +0200

    Fix wrong packet id

--- protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/packets/PlayerLook.java
+++ protocolize-data-bundle/src/main/java/dev/simplix/protocolize/data/packets/PlayerLook.java
@@ -40,7 +40,7 @@ public class PlayerLook extends AbstractPacket {
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_19_1, MINECRAFT_1_19_2, 0x16),
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_19_3, MINECRAFT_1_19_3, 0x15),
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_19_4, MINECRAFT_1_20_1, 0x16),
-        AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_2, MINECRAFT_LATEST, 0x17)
+        AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_2, MINECRAFT_LATEST, 0x18)
     );
 
     private float yaw;
